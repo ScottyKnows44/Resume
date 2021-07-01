68bead5e347fc7cf312341cf1608d57913378043
ScottyKnows44 <scottcfalcon@gmail.com>
2021-06-30 19:33:44 -0700

    Added content for Projects and intrests with CSS

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,11 @@ import './css/resume.css';
 import './css/resume.min.css';
 import profile from '../src/img/profile.jpeg';
 import { GoMarkGithub } from 'react-icons/go';
-import { AiFillLinkedin} from 'react-icons/ai';
+import {IoLogoCss3} from 'react-icons/io';
+import { AiFillHtml5, AiFillLinkedin, AiOutlineConsoleSql } from 'react-icons/ai';
+import { FaReact, FaJava, FaPhp } from 'react-icons/fa';
+import { DiJavascript1} from 'react-icons/di';
+import { SiTypescript , SiBootstrap } from 'react-icons/si';
 
 function App() {
   return (
@@ -14,14 +18,14 @@ function App() {
       <a className="navbar-brand js-scroll-trigger" href="#page-top">
         <span className="d-block d-lg-none">Scott Thompson</span>
         <span className="d-none d-lg-block">
-          <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={profile} alt="Hello" />
+          <img className="img-fluid img-thumbnail rounded-circle mx-auto mb-2" src={profile} alt="Hello" />
         </span>
       </a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav">
+        <ul className="navbar-nav textSide">
           <li className="nav-item">
             <a className="nav-link js-scroll-trigger" href="#about">About</a>
           </li>
@@ -35,7 +39,7 @@ function App() {
             <a className="nav-link js-scroll-trigger" href="#skills">Skills</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link js-scroll-trigger" href="#interests">Interests</a>
+            <a className="nav-link js-scroll-trigger" href="#interests">activities</a>
           </li>
           
         </ul>
@@ -49,10 +53,10 @@ function App() {
           <h1 className="mb-0">Scott 
             <span className="text-primary"> Thompson</span>
           </h1>
-          <div className="subheading mb-5">11827 SE 321st PL Auburn, WA 98092 · (253) 217-8029 ·
-            <a href="mailto:name@email.com">Sthompson42@mail.greenriver.edu</a>
+          <div className="subheading mb-5 mt-3">Contact me at: (253)-217-8029 -
+            <a href="mailto:name@email.com">Scottcfalcon@gmail.com</a>
           </div>
-          <p className="lead mb-5">Hard working Junior Developer dedicated to learning what life has to give. Born with natural leadership skills and a love of Software that can adapt in any company culture. </p>
+          <p className="lead mb-5">Hard working Developer dedicated to learning what life has to give. Born with natural leadership skills and a love of Software that can adapt in any company culture. </p>
           <div className="social-icons">
             <a href="https://www.linkedin.com/in/scott-thompson-087508158/">
               <GoMarkGithub />
@@ -73,31 +77,37 @@ function App() {
 
         <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
           <div className="resume-content">
-            <h3 className="mb-0">Lift driver</h3>
-            <div className="subheading mb-3">2018 - present</div>
-            <p>Pick up clients and safely transport them to there destination all while keeping a professional attitude and being friendly.  </p>
-          </div>
-
-        </div>
-
-        <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-          <div className="resume-content">
-            <h3 className="mb-0">BarTender</h3>
-            <div className="subheading mb-3">2015-2018</div>
-            <p>Provide excellent service and drinks to customers while being friendly. Maintaining a high cleaning standards with the advance knowledge and skill of making a large assortment of drinks. All at a fast pace to make sure the best service can be achieved.</p>
+            <h3 className="mb-0">Cheapreats - Web Developer </h3>
+            <div className="subheading mb-3">2020 - present</div>
+            <ul className="noBullets">
+              <li className="list-item"> Creating new dynamic React components in Storybook, integrating with Gatsby.</li>
+              <li className="list-item"> Providing consistent work that passes peer review.</li>
+              <li className="list-item"> Present/ Demo work weekly, and incorporate client feedback into the product.</li>
+              <li className="list-item"> Learning and applying new technologies such as React Hooks, Gatsby, and Storybook.</li>
+            </ul>  
           </div>
         </div>
 
         <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
           <div className="resume-content">
-            <h3 className="mb-0">WareHouse worker/ Fedex</h3>
-            <div className="subheading mb-3">2011-2015</div>
-            <p>Work in a group setting to make sure trucks are filled neatly and orderly to be sent off on time. Working well with others to keep a clean environment as to not damage any product.</p>
+            <h3 className="mb-0">Dayside LLC: Management Consulting Firm - Web Developer </h3>
+            <div className="subheading mb-3">2019 - 2020</div>
+            <ul className="noBullets">
+              <li className="list-item ">Established a web presence for a new startup, working directly for the founder.</li>
+              <li className="list-item"> Built website using PHP, My SQL, Bootstrap, including user authentication and lead generation “Contact Us” form.</li>
+              <li className="list-item">Completed project in five two-week sprints to get client feedback and evolve the design to the client's satisfaction.</li>
+              <li className="list-item">Site won a local design competition and is actively used by client.</li>
+            </ul>  
           </div>
         </div>
 
+        <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div className="resume-content">
+            <h3 className="mb-0">Transportation, Hospitality and Service Industry</h3>
+            <div className="subheading mb-3">2010-2020</div>
+          </div>
+        </div>
       </div>
-
     </section>
 
     <hr className="m-0" />
@@ -108,18 +118,19 @@ function App() {
 
     <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
       <div className="resume-content">
-        <h3 className="mb-0">Green River College Bachelor's Program</h3>
+        <h3 className="mb-0">Green River College</h3>
+        <div className="mb-0 mt-2 fontSizeForEducation">Bachelor's In Computer Science</div>
         <div className="subheading mb-3"></div>
-        <div>2019 - present</div>
-        <p>Associate Degree in Criminal Justice, earned in 2017 </p>
+        <h5>2019 - 2021</h5>
+        <p className="mt-1 fontSizeForEducation">Associate Degree in Criminal Justice, earned in 2017 </p>
       </div>
     </div>
 
     <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
       <div className="resume-content">
         <h3 className="mb-0">Code Fellows</h3>
-        <div>2018</div>
-        <p>Completed the 200 and 300 courses</p>
+        <h5>2018</h5>
+        <p className="fontSizeForEducation">Completed the 200 and 300 courses</p>
       </div>
     </div>
 
@@ -142,46 +153,62 @@ function App() {
         <div className="subheading mb-3">Programming Languages</div>
         <ul className="list-inline dev-icons">
           <li className="list-inline-item pr-2 pl-2">
-             HTML 
+            <AiFillHtml5 />
           </li>
           <li className="list-inline-item pr-2 pl-2">
-           CSS
+           <IoLogoCss3 /> 
           </li>
           <li className="list-inline-item pr-2 pl-2">
-           React
+           <FaReact />
           </li>
           <li className="list-inline-item pr-2 pl-2">
-            Java
+            <FaJava />
           </li>
           <li className="list-inline-item pr-2 pl-2">
-            Javascript
+            <DiJavascript1 />
           </li>
           <li className="list-inline-item pr-2 pl-2">
-            SQL
+            <AiOutlineConsoleSql />
           </li>
           <li className="list-inline-item pr-2 pl-2">
-            Typescript
+            <SiTypescript />
+          </li>
+          <li className="list-inline-item pr-2 pl-2">
+            <FaPhp />
+          </li>
+          <li className="list-inline-item pr-2 pl-2">
+            < SiBootstrap />
           </li>
         </ul>
 
-        <div className="subheading mb-3">Projects</div>
-        <div>Click to view project in GitHub or visit the Website</div>
+        <div className="subheading mt-5">Projects</div>
+        <div className="mb-5">Click to view project in GitHub or visit the Website</div>
         
-        <ul className="fa-ul mb-0 noBullets">
-          <li>
-            <a href="http://www.daysidellc.com/"><p> DaySideLLC Website</p></a>
+        <ul className="fa-ul mb-5 noBullets">
+          <li >
+            <a className="h5" href="http://www.daysidellc.com/"><p> DaySideLLC Website</p></a>
+            <ul className="fa-ul mb-5 noBullets">
+              <li>Languages used: HTML, CSS, Bootstrap, PHP, SQL</li>
+              <li>Collaborated with 3 other people.</li>
+              <li>Positive client feedback.</li>
+            </ul>
           </li>
 
           <li>
-              <a href="https://github.com/rogers-ch/cheapreats-table-mgmt"><p>React Application</p></a>
+              <a className="h5" href="https://github.com/rogers-ch/cheapreats-table-mgmt"><p>React Application</p></a>
+              <ul className="fa-ul mb-5 noBullets">
+              <li>Languages used: React, Hooks, JSX, Typescript, Storybook, Styled Components</li>
+              <li>Collaborated with 2 other people.</li>
+              <li>Internship/Mentorship with CheaprEats.</li>
+            </ul>
           </li>
 
           <li>
-          <a href="https://github.com/jimmychang94/catOutOfTheBag"><p>Fun Card Game</p></a>
-          </li>
-
-          <li>
-           <a href="https://github.com/Final-Project-301"><p>API Webpage</p></a>
+           <a className="h5" href="https://github.com/Final-Project-301"><p>API Book Webpage</p></a>
+           <ul className="fa-ul mb-5 noBullets">
+              <li>Languages used: HTML, CSS, JavaScript, jquery, SQL, Handlebars</li>
+              <li>Collaborated project with 3 other people</li>
+            </ul>
           </li>
 
         </ul>
@@ -191,15 +218,21 @@ function App() {
 
 <section className="resume-section p-3 p-lg-5 d-flex align-items-center mr-auto ml-auto " id="interests">
   <div className="w-100">
-    <h2 className="mb-5">Interests</h2>
-    <p>When I am not Programming I love to do many things. First is that I have a great passion for cooking, being able to cook everything from burgers to pasta. I am also a fan of music, I have been playing guitar for 10 years and I continue to play it when I relax.</p>
-    <p>Finally, I have a deep love of the outdoors. Being in scouting for 12 years I have earned the rank of Eagle which is the highest award you can earn in the program. In that program I have many stories of camping in the woods with friends and also gathering 1,200 pounds of food which was donated to charity.  </p>
+    <h2 className="mb-5">Activities</h2>
+    <div className="col-sm text-center" >
+                <p className="fontForAboutMePage">I am just your friendly neighborhood programmer.</p>
+                <p className="fontForAboutMePage">Born and raised in Washington. I spend most of my time listening to music while programming.</p>
+                <p className="fontForAboutMePage">Graduated Green River College in June 2021.</p>
+                <p className="fontForAboutMePage">Eagle Scout, received letters from past presidents.</p>
+                <p className="fontForAboutMePage">During Football season you can find me on Sundays glued to the TV watching the game. Go Seahawks!</p>
+                <p className="fontForAboutMePage">Strong love of playing guitar with friends.</p>
+                <p className="fontForAboutMePage">Skilled cook in the kitchen. I can cook up a mean steak.</p>
+            </div>
   </div>
 </section>
 
 <hr className="m-0"/>
       </div>
-    
     </div>
   );
 }
